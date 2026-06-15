06ac0aff25666a64a3c05854bd4a7ceb58f396ea
Lalit Hirvey <lalithirvey838@gmail.com>
2026-06-15 20:02:52 +0530

    Initial commit

---
+++ DOM/DAY 1/script.js
@@ -0,0 +1,29 @@
+// SELECT a TAG
+
+
+// Selection of an element
+// const h1 = document.querySelector('h1')  // select first h1 of html
+
+// h1.textContent = 'heyy' // change test of H1
+
+// let h1 = document.querySelectorAll('h1') // for selecting all tags
+
+
+// const h1 = document.getElementByTagname ('h1'); // for select all tag.
+
+
+// const h1 = getElementById ('heading'); // select element by their ID.
+
+// Universal Selectory and most use selector
+// const h1 = document.querySelector('');
+
+
+
+//              Changing of HTML 
+
+// TEXT Change -->
+// h1.innerHTML = 'polo';
+// h1.innertext = 'polo'
+// h1.textContent = 'polo';
+
+
---
+++ DOM/DAY 2/script.js
@@ -0,0 +1,65 @@
+// const body = document.body;
+// const h1 = document.querySelector("h1");
+
+
+// const body = document.body;
+// const main = document.querySelector("main");
+// console.log(main.childNodes);
+// console.log(main.children);
+
+
+
+
+// const h1 =  document.querySelector("h1");
+// console.log(h1);
+// // h1.textContent = "Yokoso" // only change or update text.
+// // h1.innerText; // change the inner text in h1.
+// h1.innerHTML; // pure html ko change kar dega
+
+
+
+// h1.style.background = "red";
+// h1.style.fontStyle = "red";
+// h1.style.fontSize = "30px";
+// h1.style.fontStyle = "italic"
+// h1.textContent = "hey I am change"
+// h1.style.color = "red"
+
+
+
+
+
+            // CLASSES 
+    // 1. classlist.add() // add class to 
+    // 2. classlist.remove() // delete
+    // 3. contain() // True or False
+    // 4. toggle() //// is exist = remove.+, or is not exist = add
+    // 5. replaace() // take (class, New)
+
+ // for searching.
+//     const isClass = h1.classList.contains("heading")
+//     console.log(isClass);
+
+
+// h1.classList.toggle("heading");
+// console.log(h1);
+
+
+
+// const buld = document.querySelector(".buld");
+// const btn = document.querySelector("button");
+// let flag = true;
+// btn.addEventListener('click', function (){
+//     if (flag) {
+//     bulb.style.backgroundColor = "yellow"
+//     btn.textContent = "Off";
+//     flag = false;
+// } else {
+//      bulb.style.backgroundColor = "black"
+//     btn.textContent = "On";
+//     flag = true;
+// }
+// }) ;
+
+
+
